a593c576035c1076a29e9323633e612453bf028d
MarkoAndelic <marko92andelic@gmail.com>
2018-08-06 20:02:50 +0200

    Dodano izracun potrošnje plina i cijena puta

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function izracun(){
 	var prijedenikm =document.getElementById("prijedenikm").value;
 	var natocenoGorivo = document.getElementById("natoceno").value;
 	var cijenagoriva = document.getElementById("cijenalgoriva").value;
+	var cijenaplina = document.getElementById("cijenalplina").value;
 	var rezultat = prijedenikm / (natocenoGorivo/cijenagoriva);
 
-	document.getElementById("ispisRezultata").innerHTML = rezultat;
+	document.getElementById("ispisRezultata").innerHTML = rezultat.toFixed(2) + " l/km";
 
 	var potrosnjaPlin = rezultat + (rezultat * 0.10);
 	
-	document.getElementById("plincijena").innerHTML = potrosnjaPlin;
+	document.getElementById("potrosnjaPlina").innerHTML = potrosnjaPlin.toFixed(2) + " l/km";
+
+	var cijenaVoznjeNaPlin =(prijedenikm / potrosnjaPlin) * cijenaplina;
+
+	document.getElementById("cijnaVoznjeNaPlin").innerHTML = cijenaVoznjeNaPlin.toFixed(2) + " kn";
 }
+
 
